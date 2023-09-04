02bd6dd927283cd1940047d93321bb99f325e704
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-09-04 12:56:08 -0300

    feat: Alterar o texto da tela ao apertar nos btns

--- script.js
+++ script.js
@@ -2,12 +2,35 @@ const html = document.querySelector('html')
 const buttonFoco = document.querySelector('.app__card-button--foco')
 const buttonDescansoCurto = document.querySelector('.app__card-button--curto')
 const buttonDescansoLongo = document.querySelector('.app__card-button--longo')
-
 const imagemPersonagem = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+
 
 function alterarContexto(contexto){
     html.setAttribute('data-contexto', contexto)
     imagemPersonagem.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
+            break;
+        default:
+            break;
+    }
 }
 
 buttonFoco.addEventListener('click', () => {
