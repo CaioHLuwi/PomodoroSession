7f6db6eea62deb5a59abecb4af3651e32fea555a
CaioHLuwi <caiohenriquelw2004@gmail.com>
2023-09-04 12:39:40 -0300

    feat: Mudar imagem de acordo com botões

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+/* Mudança de background */
 const html = document.querySelector('html')
 const buttonFoco = document.querySelector('.app__card-button--foco')
 const buttonDescansoCurto = document.querySelector('.app__card-button--curto')
 const buttonDescansoLongo = document.querySelector('.app__card-button--longo')
 
+const imagemPersonagem = document.querySelector('.app__image')
+
 buttonFoco.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
+    imagemPersonagem.setAttribute('src', '/imagens/foco.png')
 })
 
 buttonDescansoCurto.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+    imagemPersonagem.setAttribute('src', '/imagens/descanso-curto.png')
 })
 
 buttonDescansoLongo.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
+    imagemPersonagem.setAttribute('src', '/imagens/descanso-longo.png')
 })
+/* Mudança de background */
+
+
